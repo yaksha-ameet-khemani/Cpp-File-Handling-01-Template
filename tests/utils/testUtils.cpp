576e290684a2d7008c3a442e3fab9a21cbffa3a4
Ameet Khemani <ameettechademy@gmail.com>
2023-05-08 17:55:58 +0530

    updated url endpoint

--- tests/utils/testUtils.cpp
+++ tests/utils/testUtils.cpp
@@ -46,174 +46,45 @@ class TestUtils {
 				resultScore = 1;
 				resultStatus = "Passed";
 			}
-			// try {
-				string testTypeB = "functional";
-				// if (file.getName().contains("boundary"))
-				// 	testType = "boundary";
-				// if (file.getName().contains("exception"))
-				// 	testType = "exception";
-				// map["one"] = 1;
-				// TestCaseResult testCaseResultObj(testName, testType, 1, resultScore, resultStatus, true, "");
-				testCaseResults.insert(std::make_pair(GUID, TestCaseResult(testName, testType, 1, resultScore, resultStatus, true, "")));
-				// cout<<testCaseResults.size();
-				// map<std::string, TestCaseResult>::iterator it;
-				// for (it = testCaseResults.begin(); it != testCaseResults.end(); it++) {
-				// 	std::cout << it->first    // string (key)
-				// 			<< ':'
-				// 			<< it->second.getMethodName()   // string's value 
-				// 			<< std::endl;
-				// }
-
-			// } catch (...) {
-			// // TODO Auto-generated catch block
-			// 	cout<<"error in assertion";
-			// }
+			string testTypeB = "functional";
+			testCaseResults.insert(std::make_pair(GUID, TestCaseResult(testName, testType, 1, resultScore, resultStatus, true, "")));
 			testResults.setTestCaseResults(testCaseResultAsJsonString(GUID, TestCaseResult(testName, testType, 1, resultScore, resultStatus, true, "")));
 			testResults.setCustomData(customData);
 			string finalResult = testResultsAsJsonString(testResults);
-			
 			curlpp::Cleanup cleanup;
 			curlpp::Easy request;
-			request.setOpt(curlpp::options::Url(std::string("https://jsonplaceholder.typicode.com/posts")));
+			request.setOpt(curlpp::options::Url(std::string("https://yaksha-prod-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=jSTWTxtQ8kZgQ5FC0oLgoSgZG7UoU9Asnmxgp6hLLvYId/GW9ccoLw==")));
 			std::list<std::string> header = {
 				"Content-Type: application/json",
 				"accept: application/json"
 			}; 
-			request.setOpt(new curlpp::options::HttpHeader(header));
-			
-			std::string query = finalResult;// filePresent ? "[{\"FilePresentTrue\":1}]" : "[{\"FilePresentTrue\":0}]";
+			request.setOpt(new curlpp::options::HttpHeader(header));			
+			std::string query = finalResult;
 			request.setOpt(new curlpp::options::PostFields(query)); 
 			request.setOpt(new curlpp::options::WriteStream(&std::cout));
 			request.perform();
-			
-			// cout<<finalResult;
-			// System.out.println(finalResult);
-			// HttpClient client = HttpClient.newHttpClient();
 		}
 
-    /*
-	public static void yakshaAssert(String testName, Object result, File file) throws IOException {
-		TestResults testResults = new TestResults();
-		Map<String, TestCaseResultDto> testCaseResults = new HashMap<String, TestCaseResultDto>();
-
-		customData = readData("../custom.ih");
-		String resultStatus = "Failed";
-		int resultScore = 0;
-		if (result.toString().equals("true")) {
-			resultScore = 1;
-			resultStatus = "Passed";
-		}
-		try {
-			String testType = "functional";
-			if (file.getName().contains("boundary"))
-				testType = "boundary";
-			if (file.getName().contains("exception"))
-				testType = "exception";
-			testCaseResults.put(GUID,
-					new TestCaseResultDto(testName, testType, 1, resultScore, resultStatus, true, ""));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		}
-		testResults.setTestCaseResults(asJsonString(testCaseResults));
-		testResults.setCustomData(customData);
-
-		String finalResult = asJsonString(testResults);
-		System.out.println(finalResult);
-//		HttpClient client = HttpClient.newHttpClient();
-//	        HttpRequest request = HttpRequest.newBuilder()
-//	                .uri(URI.create(URL))
-//	                .POST(HttpRequest.BodyPublishers.ofString(finalResult, Charset.defaultCharset()))
-//	                .build();
-//
-//	        try {
-//				HttpResponse<String> response = client.send(request,
-//				        HttpResponse.BodyHandlers.ofString());
-//			} catch (IOException | InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//				System.out.println(e);
-//			}
-
-		URL url = new URL(URL);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setDoOutput(true);
-		conn.setRequestMethod("POST");
-		conn.setRequestProperty("Content-Type", "application/json");
-
-		String input = asJsonString(testResults);
-
-		OutputStream os = conn.getOutputStream();
-		os.write(input.getBytes());
-		os.flush();
-
-		if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
-			throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+		string testCaseResultAsJsonString(string GUID, TestCaseResult testCaseResult) {
+			json j = {
+				{GUID, {
+					{"methodName", testCaseResult.getMethodName()},
+					{"methodType", testCaseResult.getMethodType()},
+					{"actualScore", testCaseResult.getActualScore()},
+					{"earnedScore", testCaseResult.getEarnedScore()},
+					{"status", testCaseResult.getStatus()},
+					{"isMandatory", testCaseResult.getIsMandatory()},
+					{"errorMessage", testCaseResult.getErroMessage()}
+				}},
+			};
+			return j.dump();
 		}
 
-		BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-		String output;
-		System.out.println("Output from Server .... \n");
-		while ((output = br.readLine()) != null) {
-			System.out.println(output);
+		string testResultsAsJsonString(TestResults testResults) {
+			json j = {
+				{"testCaseResults", testResults.getTestCaseResults()},
+				{"customData", testResults.getCustomData()}
+			};
+			return j.dump();
 		}
-
-		total++;
-		String[] r = testName.split("(?=\\p{Upper})");
-		System.out.print("\n" + BLUE_BOLD_BRIGHT + "=>");
-		// testResult = testResult + "\n" + BLUE_BOLD_BRIGHT + "=>";
-
-		System.out.print(YELLOW_BOLD_BRIGHT + "Test For : ");
-		// testResult = testResult + YELLOW_BOLD_BRIGHT + "Test For : ";
-
-		for (int i = 1; i < r.length; i++) {
-			System.out.print(YELLOW_BOLD_BRIGHT + r[i] + " ");
-			// testResult = testResult + YELLOW_BOLD_BRIGHT + r[i] + " ";
-		}
-		System.out.print(" : ");
-		// testResult = testResult + " : ";
-		if (result.toString().equals("true")) {
-			System.out.println(GREEN_BOLD_BRIGHT + "PASSED" + TEXT_RESET);
-			// testResult = testResult + GREEN_BOLD_BRIGHT + "PASSED" + TEXT_RESET;
-			passed++;
-		} else {
-			System.out.println(RED_BOLD_BRIGHT + "FAILED" + TEXT_RESET);
-			// testResult = testResult + RED_BOLD_BRIGHT + "FAILED" + TEXT_RESET;
-			failed++;
-		}
-//			FileWriter writer = new FileWriter(file,true);
-//			writer.append("\n" + testName + "=" + result);
-//			writer.flush();
-//			writer.close();
-		// createXML(testName, file);
-
-	}
-    */
-
-	string testCaseResultAsJsonString(string GUID, TestCaseResult testCaseResult) {
-		json j = {
-			{GUID, {
-				{"methodName", testCaseResult.getMethodName()},
-				{"methodType", testCaseResult.getMethodType()},
-				{"actualScore", testCaseResult.getActualScore()},
-				{"earnedScore", testCaseResult.getEarnedScore()},
-				{"status", testCaseResult.getStatus()},
-				{"isMandatory", testCaseResult.getIsMandatory()},
-				{"errorMessage", testCaseResult.getErroMessage()}
-			}},
-		};
-		// return j.dump(4);
-		return j.dump();
-	}
-
-	string testResultsAsJsonString(TestResults testResults) {
-		json j = {
-			{"testCaseResults", testResults.getTestCaseResults()},
-			{"customData", testResults.getCustomData()}
-		};
-		return j.dump(4);
-		// return j.dump();
-	}
 };
